68f2a22887565015f81f9a81ba2891a61cc2bd22
Romain <94350004+RomainPrbl@users.noreply.github.com>
2022-05-03 13:18:15 +0200

    Create fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,18 @@
+#include stdio.h
+#include stdlib.h
+
+int main (void) {
+	printf("hello");
+    for (int i = 1; i = 100; i++) {
+        if (i % 15 == 0) {
+            printf (FizzBuzzn);
+        } else if (i % 3 == 0) {
+            printf (UwUn);
+        } else if (i % 5 == 0) {
+            printf (Buzzn);
+        } else {
+            printf (%dn, i);
+        }
+    }
+    return EXIT_SUCCESS;
+} 
